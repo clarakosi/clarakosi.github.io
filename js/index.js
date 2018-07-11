2f505502f1162d2b4ccc1dac547e047e542ecb28
Clara Andrew <candrewwani@gmail.com>
2018-07-11 16:40:04 -0400

    added main files
    Added html, css, and js files for portfolio site.

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// Animations initialization
+new WOW().init();
+
+const typed = new Typed('#typed', {
+  stringsElement: '#typed-strings',
+  typeSpeed: 50,
+  backSpeed: 50,
+  smartBackspace: true
+});
